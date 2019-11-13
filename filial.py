79233e32bb11f27b79727fdf866423e7d02f183c
andrey <d@mail.ru>
2019-11-13 19:43:23 +0900

    1

--- filial.py
+++ filial.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 import logging
-from config import filial_config,filial_logname, filial_logname_debug, filial_logname_error
+from config import filial_config,filial_logname, filial_logname_debug, filial_logname_error, filial_tovar_group_id, \
+    filial_tovar_group_name
 from sql import SqlClient
 from wsdl import WsdlClient
 import logs
@@ -22,8 +23,8 @@ conn = sql_client.conn
 
 #загрузка корневой группы товаров
 tovar_group_list = []
-nom_group = wsdl_client.nomenklatura_type(code='', name="!ТОВАРЫ ФИЛИАЛ ЗЕЯ",
-                                          id='3b5374ea-0bcc-4d4d-bae6-971e2e748505', idparent='')
+nom_group = wsdl_client.nomenklatura_type(code='', name=filial_tovar_group_name,
+                                          id=filial_tovar_group_id, idparent='')
 tovar_group_list.append(nom_group)
 arrayn_group = wsdl_client.arrayn_type(nomenklatura=tovar_group_list)
 wsdl_client.client.service.load_nom_groups(arrayn_group, 1)
