6c9cdcf0f5b754ee674cce30389285a720db7257
andrey <d@mail.ru>
2020-01-07 00:07:53 +0900

    1

--- main.py
+++ main.py
@@ -115,27 +115,25 @@ while True:
                 logging.warning(['Загрузка списка документов'])
     elif k == 'авто':
         white_list = []
-        if 1 == 1:
-# расход
-            white_list.append(3716) #расходнаядоставка
-            white_list.append(410) #расходнаянакладная
-            white_list.append(469) #расходнаяреализатора
+        if 1 == 0:
+            white_list.append(3716)     # расходнаядоставка
+            white_list.append(410)      # расходнаянакладная
+            white_list.append(469)      # расходнаяреализатора
 
-            white_list.append(33) #номенклатура
+            white_list.append(33)       # номенклатура
         
-            white_list.append(239) #перемещение
-            white_list.append(310) #ввод остатков
-            white_list.append(434) #приход
-            white_list.append(2989) #движенияденежныхсредств
-            white_list.append(4308) #выручкадоставка
-            white_list.append(2964) #ПриходныйОрдерТБ
-            white_list.append(4179) #АктПереоценкиКлиенты
-            white_list.append(4225) #РасходныйОрдерТБ
-        #if 1==1:
+            white_list.append(239)      # перемещение
+            white_list.append(310)      # ввод остатков
+            white_list.append(434)      # приход
+            white_list.append(2989)     # движенияденежныхсредств
+            white_list.append(4308)     # выручкадоставка
+            white_list.append(2964)     # ПриходныйОрдерТБ
+            white_list.append(4179)     # АктПереоценкиКлиенты
+            white_list.append(4225)     # РасходныйОрдерТБ
+            white_list.append(297)      # списания
 
-            #взаиморасчеты
-        if 1==1:
-            white_list.append(297) #списания
+        if 1 == 1:
+            white_list.append(4308)     # выручкадоставка
 
         while True:
             logging.warning('Выборка изменений')
@@ -155,7 +153,7 @@ while True:
                     pass
                 # номенклатура
                 if row_delta['TYPEID'] == 33:
-                    str_id=",".join(["'"+row_delta['OBJID']+"'"])
+                    str_id = ",".join(["'"+row_delta['OBJID']+"'"])
                     nomenklatura.load_nomenklatura(cursor, prm_id_str=str_id, prm_id_mode=1, prm_with_parent=0,
                                                    prm_update_mode=1, wsdl_client=wsdl_client)
                 
@@ -173,62 +171,61 @@ while True:
 
                 # взаиморасчеты
                 elif row_delta['TYPEID'] in [2989, 4308, 2964, 4179, 4225]: #взаиморасчеты
-                    #взаиморасчеты
-                    #2989 - движенияденежныхсредств
-                    #4308 - выручкадоставка SP6083
-                    #2964 - ПриходныйОрдерТБ Прих.орд.(торг.) SP6084
-                    #4179 - АктПереоценкиКлиенты Акт переоц. SP6085
-                    #4225 РасходныйОрдерТБ SP6082
-                    logging.info(';'.join(['Выборка взаиморасчетов',str(row_delta['TYPEID']),row_delta['OBJID']]))
-                    if (row_delta['TYPEID']==2989) :
-                        idartmarket_str='SP6081'
-                        doc_descr='Выписка банка'
-                    elif (row_delta['TYPEID']==4308) :
-                        idartmarket_str='SP6083'
-                        doc_descr='Выручка доставка'
-                    elif (row_delta['TYPEID']==2964) :
-                        idartmarket_str='SP6084'
+                    # взаиморасчеты
+                    # 2989 - движенияденежныхсредств
+                    # 4308 - выручкадоставка SP6083
+                    # 2964 - ПриходныйОрдерТБ Прих.орд.(торг.) SP6084
+                    # 4179 - АктПереоценкиКлиенты Акт переоц. SP6085
+                    # 4225 РасходныйОрдерТБ SP6082
+                    logging.info(';'.join(['Выборка взаиморасчетов', str(row_delta['TYPEID']), row_delta['OBJID']]))
+                    if row_delta['TYPEID'] == 2989:
+                        idartmarket_str = 'SP6081'
+                        doc_descr = 'Выписка банка'
+                    elif row_delta['TYPEID'] == 4308:
+                        idartmarket_str = 'SP6083'
+                        doc_descr = 'Выручка доставка'
+                    elif row_delta['TYPEID'] == 2964:
+                        idartmarket_str = 'SP6084'
                         doc_descr='Прих.орд.(торг.)'
-                    elif (row_delta['TYPEID']==4179) :
-                        idartmarket_str='SP6085'
-                        doc_descr='Акт переоц.'
-                    elif (row_delta['TYPEID']==4225) :
-                        idartmarket_str='SP6082'
-                        doc_descr='Расх.орд.(торг.)'
+                    elif row_delta['TYPEID'] == 4179:
+                        idartmarket_str = 'SP6085'
+                        doc_descr = 'Акт переоц.'
+                    elif row_delta['TYPEID'] == 4225:
+                        idartmarket_str = 'SP6082'
+                        doc_descr = 'Расх.орд.(торг.)'
 
-                    select_str='''
+                    select_str = '''
                     SELECT   closed, CAST(LEFT(Date_Time_IDDoc, 8) as DateTime) as datedoc,docno,
                     sc13.sp4805 as firma,
                     '''+idartmarket_str+''' as idartmarket,
                     _1sjourn.iddoc FROM DH'''+str(row_delta['TYPEID'])+''' as dh
                     left join _1sjourn on dh.iddoc=_1sjourn.iddoc 
                     left join sc13 on SP1005=sc13.id
-                    where _1sjourn.iddoc=%s
+                    where _1sjourn.iddoc=%s and _1sjourn.iddocdef=%s
                     '''    
-                    cursor.execute(select_str,row_delta['OBJID'])  
+                    cursor.execute(select_str, (row_delta['OBJID'], row_delta['TYPEID']))
                     
-                    rows_header=cursor.fetchall()
+                    rows_header = cursor.fetchall()
 
-                    logging.info('Выборка взаиморасчетов завершена')
-
+                    logging.info('Выборка взаиморасчетов')
 
                     for row in rows_header:
-                        client_list=[]
-                        isclosed=row['closed'] and 1
-                        if isclosed!=1:
-                            #continue
+                        client_list = []
+                        isclosed = row['closed'] and 1
+                        if isclosed != 1:
                             pass
+                        logging.info(['Выборка взаиморасчетов опмание объекта', row['datedoc'], row['docno']])
 
                         if row['firma'] != '9CD36F19-B8BD-49BC-BED4-A3335D2175C2    ':
                             continue
-                        if row['idartmarket'] == None or row['idartmarket'].strip() == '':
+                        if row['idartmarket'] is None or row['idartmarket'].strip() == '':
                             if isclosed == 1:
                                 logging.error(';'.join(['Пустой ид', row['docno']]))
                             continue
 
                         header = wsdl_client.header_type(document_type=2, firma=row['firma'].strip(), sklad=doc_descr,
-                                                       client='', idartmarket=row['idartmarket'].strip(),
-                                                       document_date=row['datedoc'], nomerartmarket=row['docno'])
+                                                         client='', idartmarket=row['idartmarket'].strip(),
+                                                         document_date=row['datedoc'], nomerartmarket=row['docno'])
 
                         
                         #расход debkred 1
@@ -250,8 +247,8 @@ while True:
                         #debkred 1 уменьшение долга клиента
                         #debkred 0 увеличение долга клиента
                         cursor.execute('''
-                                        select sc46.sp4807 as client,debkred, SP936 as summa,
-                                        1 as typedvig from ra933
+                                        select sc46.sp4807 as client, debkred, SP936 as summa,
+                                        1 as typedvig, SP4373 as docosnov from ra933
                                         left join sc46 on SP934 = sc46.id
                                         where ra933.iddoc=%s
                                             ''', row['iddoc'])
@@ -263,13 +260,14 @@ while True:
                         client_list = []
                         row_list = []
 
-
-
                         for row_table in rows_table:
+                            docosnov_list = row_table['docosnov'].strip().split(' ')
+                            logging.warning(convert_base(docosnov_list[0], from_base=36))
+                            logging.warning([row_table['docosnov'], docosnov_list])
                             if row_table['debkred']:
-                                debkred=1
+                                debkred = 1
                             else:
-                                debkred=2
+                                debkred = 2
 
                             if debkred == 1 and row_table['typedvig'] == 1:
                                 logging.error(';'.join(['контроль операции', row['docno']]))
@@ -281,11 +279,11 @@ while True:
                             if not "'"+row_table['client']+"'" in client_list:
                                 client_list.append("'"+row_table['client']+"'")
                             row_list.append(row_nom)
-                        if client_list==[]:
+                        if not client_list:
                             continue
                         str_id = ",".join(client_list)
-                        hdb.get_client_groups(wsdl_client,cursor,str_id)
-                        #print(row_list)
+                        hdb.get_client_groups(wsdl_client, cursor, str_id)
+                        # print(row_list)
                         rows = wsdl_client.rows_type(rows=row_list)
                         document = wsdl_client.document_type(header=header,rowslist=rows)
                         logging.info(';'.join(['Загрузка документа взаиморасчетов',row['docno']]))
