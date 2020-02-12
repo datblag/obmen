e471108fc636a03dae8082c8eaa08807f1612c51
andrey <d@mail.ru>
2020-02-12 19:52:37 +0900

    контроли

--- doc_control.py
+++ doc_control.py
@@ -8,9 +8,13 @@ from rashod import load_rashod
 
 def check_rashod(cursor, wsdl_client):
     check_list = []
-    check_list.append({'doctype': 410, 'idartmarket': 'SP6060'})
-    check_list.append({'doctype': 469, 'idartmarket': 'SP6072'})
-    check_list.append({'doctype': 3716, 'idartmarket': 'SP6071'})
+    # check_list.append({'doctype': 410, 'idartmarket': 'SP6060'})
+    # check_list.append({'doctype': 469, 'idartmarket': 'SP6072'})
+    # check_list.append({'doctype': 3716, 'idartmarket': 'SP6071'})
+    # check_list.append({'doctype': 297, 'idartmarket': 'SP6076'})  # списание
+    # check_list.append({'doctype': 434, 'idartmarket': 'SP6059'})  # приход
+    check_list.append({'doctype': 239, 'idartmarket': 'SP6079'})  # перемещение
+    check_list.append({'doctype': 310, 'idartmarket': 'SP6077'})  # ввод остатков
     logging.warning('rashod control start')
     for check_item in check_list:
         logging.warning(check_item)
@@ -37,7 +41,7 @@ def check_rashod(cursor, wsdl_client):
             isclosed = row['closed'] and 1
             if res == -100 and isclosed == 1 or res == 100 and isclosed == 1:
                 logging.warning([res, row['datedoc'], row['docno'], isclosed, row['iddoc']])
-                load_rashod(cursor, wsdl_client, {'TYPEID': check_item['doctype'], 'OBJID': row['iddoc']})
+                # load_rashod(cursor, wsdl_client, {'TYPEID': check_item['doctype'], 'OBJID': row['iddoc']})
             elif res == 200 and not isclosed == 1:
                 logging.warning(["Ошибка проведен", res, row['datedoc'], row['docno'], isclosed, row['iddoc']])
         logging.warning('rashod control compete')
